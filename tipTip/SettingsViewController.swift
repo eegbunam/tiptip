2ae44ac6c7e60454caa0492127262ec02759a2b4
ebukaebunam <ebukaebunam@gmail.com>
2020-05-27 17:08:59 -0500

    Worked on setting View controller Views

--- tipTip/SettingsViewController.swift
+++ tipTip/SettingsViewController.swift
@@ -8,26 +8,89 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
 
+protocol settingsDelegate {
+    func didChangeTip(tip : Double)
+}
+
+class SettingsViewController: UIViewController {
+    
+    var delegate :settingsDelegate?
+  
+    var changeTipBtn : UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setTitle("Change Default tip", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont(descriptor: UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.name: "ArialRoundedMTBold"]), size: 17)
+        button.addTarget(self, action: #selector(handleChangeTip), for: .touchUpInside)
+        button.backgroundColor = .darkGray
+        
+        return button
+    }()
+    
+    
+    var tipTextField : UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.frame = .zero
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Add a default tip"
+        textField.keyboardType = .decimalPad
+        return textField
+        
+    }()
+    
+    
+    var sucessLabel : UILabel = {
+        let label = UILabel(frame: CGRect(x:0, y: 300
+            , width: 300, height: 60))
+        label.layer.cornerRadius = 15
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- navigationController?.navigationBar.prefersLargeTitles = true
-     
-         title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Settings"
+        setUpView()
+        
         
     }
     
-    var text1 : UILabel {
-        let label = UILabel()
-        label.frame = CGRect(x: 0, y: 0, width: 200, height: 40)
-        label.layer.cornerRadius = 7
-        label.font = UIFont(descriptor: UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.name: "ArialRoundedMTBold"]), size: 17)
-        label.adjustsFontSizeToFitWidth = true
+    
+    @objc func handleChangeTip(){
+        print("button is being pressed")
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "tip") as! TipViewController
+        self.delegate = vc
+
+        delegate?.didChangeTip(tip: Double(tipTextField.text!)!)
+        performSegue(withIdentifier: "goBack", sender: self)
+      
+    }
+
+    
+    
+    func setUpView(){
         
-        return label
+        let firstStack = UIStackView(arrangedSubviews: [tipTextField, changeTipBtn])
+        firstStack.translatesAutoresizingMaskIntoConstraints = false
+        firstStack.axis = .vertical
+        //Utilities.styleTextField(tipTextField)
+        view.addSubview(firstStack)
+    
+        firstStack.spacing = 20
+        firstStack.distribution = .fillEqually
+        firstStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        firstStack.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        firstStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        firstStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
     }
     
-
-
+    
+    
 }
--- tipTip/TipViewController.swift
+++ tipTip/TipViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class TipViewController: UIViewController {
     
     
@@ -18,30 +20,58 @@ class TipViewController: UIViewController {
     @IBOutlet weak var totalAmountLabel: UILabel!
     
     @IBOutlet weak var totalLabel: UILabel!
+    let defaults = UserDefaults.standard
+    
+    var defaultTip : Double?  = 12.0
     
     
+    @IBAction func unwindToVC1(segue:UIStoryboardSegue) { }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        defaults.set(billTextField.text, forKey: "mybill")
+        defaults.set(, forKey: "mybill")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let bill = billTextField.text {
+            print(defaultTip)
+            let finalBill = Double(bill)
+            let tip = Tip(bill: finalBill, percentage: defaultTip)
+            totalAmountLabel.text = tip.totalAmount
+            tipLabel.text = tip.discription
+            
+        }
+        
+    }
     
     override func viewDidLoad() {
-       // view.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
-        
         title = "Calculate Tip"
-        
-        
         navigationItem.setRightBarButton(UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleBarButton)), animated: true)
         navigationController?.navigationBar.tintColor = .black
         setUpView()
         
-        // Do any additional setup after loading the view.
     }
     
     @objc func handleBarButton(){
         
-         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "settings") as SettingsViewController
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "settings") as SettingsViewController
         
         navigationController?.pushViewController(vc, animated: true)
         
+    }
+    
+    
+    func handlechnageTip () {
+        if let bill = billTextField.text {
+            let finalBill = Double(bill)
+            let tip = Tip(bill: finalBill, percentage: defaultTip)
+            totalAmountLabel.text = tip.totalAmount
+            tipLabel.text = tip.discription
+            
+            
+        }
         
     }
     @IBAction func onTap(_ sender: Any) {
@@ -50,12 +80,9 @@ class TipViewController: UIViewController {
     
     
     @IBAction func billIsChanged(_ sender: Any) {
-        
-        
         if let bill = billTextField.text {
             let finalBill = Double(bill)
-            let tip = Tip(bill: finalBill, percentage: 10.0)
-            
+            let tip = Tip(bill: finalBill, percentage: defaultTip)
             totalAmountLabel.text = tip.totalAmount
             tipLabel.text = tip.discription
             
@@ -71,11 +98,7 @@ class TipViewController: UIViewController {
         Utilities.styleLabel(tipLabel)
         Utilities.styleLabel(totalAmountLabel)
         Utilities.styleLabel(totalLabel)
-        
     }
-    
-    
-    
 }
 
 
@@ -87,8 +110,6 @@ struct Tip {
     
     var percentage : Double? = 0.0
     
-    
-    
     init(bill :Double? , percentage : Double?) {
         self.bill = bill
         self.percentage = percentage
@@ -96,22 +117,23 @@ struct Tip {
     
     var discription : String {
         guard let percentage = percentage else {
-            return ""
+            return "10% Tip :$0.00"
             
         }
+        
         return "\(percentage)% Tip : \(tip)"
     }
     
     var tip : String {
         guard let bill = bill , let percentage = percentage else{
-            return ""
+            return "$0.00"
         }
         return  String(format: "$%.2f", percentage/100 * bill)
     }
     
     var totalAmount  : String {
         guard let bill = bill , let percentage = percentage else{
-            return ""
+            return "$0.00"
         }
         if percentage == 0.0{
             let tip =  bill + (0.1 * bill)
@@ -120,9 +142,17 @@ struct Tip {
             let tip =  bill + (percentage/100 * bill)
             return  String(format: "$%.2f", tip)
         }
+        
     }
     
     
+}
+
+extension TipViewController : settingsDelegate {
+    func didChangeTip(tip: Double) {
+  
+        
+    }
 }
 
 
